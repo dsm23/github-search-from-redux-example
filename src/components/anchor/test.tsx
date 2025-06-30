5e05ded07cb4691421ac8199d0c4ac592fc468d8
David Murdoch <dsmurdoch357@gmail.com>
2025-06-30 17:10:13 +0100

    test(unit): add unit tests to button and anchor link

---
+++ src/components/anchor/test.tsx
@@ -0,0 +1,23 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Anchor from ".";
+
+describe("component", () => {
+  describe("Button", () => {
+    it("should render correctly", () => {
+      render(<Anchor href="#">Hello, World!</Anchor>);
+
+      expect(screen.getByRole("link")).toBeInTheDocument();
+    });
+
+    it("should render correctly, asChild", () => {
+      render(
+        <Anchor asChild>
+          <button>Hello, World!</button>
+        </Anchor>,
+      );
+
+      expect(screen.getByRole("button")).toBeInTheDocument();
+    });
+  });
+});
